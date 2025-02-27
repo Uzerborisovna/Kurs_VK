b317262bceca2f00a51220fb68d4758ffcc18d77
Julia Zernova <uzerborisovna@gmail.com>
2025-02-27 20:11:49 +0300

    add new

--- KursovayaVK.py
+++ KursovayaVK.py
@@ -70,7 +70,7 @@ with open('test_write.txt', "w", encoding = "utf-8") as f:
 
 class YD:
     def __init__(self, token):
-        self.params = {'access_token': token, 'v': version}
+        self.params = {'access_token': TOKEN_, 'v': version}
         print('создался ЯД')
 
     def folder_creation(folder_name):
@@ -89,8 +89,11 @@ class YD:
         url = "https://cloud-api.yandex.net/v1/disk/resources/upload?url=https://sun9-64.userapi.com/s/v1/if2/7XjpgFYihljZ1Au22hjS93aHy0T9WT2RVMEIbmWWNppTqsJVLIEqC7uuuCQrBaIK6Lv_2ciyDfyQhbaNek8TTk9i.jpg?quality=96&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,1080x1440,1280x1707,1440x1920,1536x2048&from=bu&cs=130x173&path=https://disk.yandex.ru/client/disk/Photos"
         headers = {'Content-Type': 'application/json','Authorization': f'OAuth+TOKEN_'}
         params = {'path': f'Photos','url': link}
-        response = requests.POST(url=url, headers=headers, params=params)
+
+        response = requests.post(url=url, headers=headers, params=params)
         print(response)
+        if response.status_code != 202:
+            print(f'Ошибка на сервере. Код ошибки: {response.status_code}')
 
 
 
