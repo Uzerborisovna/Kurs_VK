ff0f2b78b874cb0932ce0a112f6aa7e6faf91ccf
Julia Zernova <uzerborisovna@gmail.com>
2025-02-27 18:45:09 +0300

    add new

--- KursovayaVK.py
+++ KursovayaVK.py
@@ -1,18 +1,20 @@
 import configparser
 import requests
 from pprint import pprint
+from datetime import datetime
 from tqdm import tqdm
 import time
 
-from tqdm import tqdm
-import time
-for i in tqdm(range(10)):
-    time.sleep(0.5)
+
+# for i in tqdm(range(10)):
+#     time.sleep(0.5)
 
 congig = configparser.ConfigParser()
 congig.read("settings.ini")
 vk_tk = congig["Tokens"]["vk_token"]
 user_id = congig["Tokens"]["user_id"]
+TOKEN_ = congig["Tokens"]["yd_token"]
+
 
 class VK:
     def __init__(self, token, version='5.199'):
@@ -54,8 +56,9 @@ for item in items:
     name = str(likes)
     url, type = vk_connector.max_size_photo(sizes)
     photo_info[name] = str({'url': url, 'type': type})
+    url_jpg = item['sizes'][1]["url"]
+    #print(url_jpg)
 
-#pprint(photo_info)
 
 with open('test_write.txt', "w", encoding = "utf-8") as f:
     for photo in photo_info:
@@ -63,18 +66,48 @@ with open('test_write.txt', "w", encoding = "utf-8") as f:
         f.write(str(photo_info[name][0]) + '\n')
         print("\n".join(map(str, photo_info)), file=f)
 
-
 class YD:
     def __init__(self, token):
         self.params = {'access_token': token, 'v': version}
         print('создался ЯД')
 
-    def create_folder(self, folder_name):
-        requests.put(f'{https://cloud-api.yandex.net/v1/disk/resources?path=%2FMy_photos}?path={path}')
-        create_folder('My_photos')
+    def folder_creation(folder_name):
+        """Создаёт папку на Яндекс Диске"""
+        url = f'https://cloud-api.yandex.net/v1/disk/resources/'
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': f'OAuth {TOKEN_}'}
+        params = {'path': folder_name, 'overwrite': 'false'}
+        response = requests.put(url=url, headers=headers, params=params)
+        print(response)
 
-    def upload_file(self, path, url):
-        for photo in photo_info:
-            upload_file(r'https://cloud-api.yandex.net/v1/disk/resources/upload?path=My_photos%2Fimage.jpg&url=https%3A%2F%2F"url"')
+        folder_creation('Photos')
+
+    def upload_social_network(self, file_name, link):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        params = {'path': f'Photos/photo_1','url':'https://sun9-35.userapi.com/c9591/u00001/136592355/m_672c7bad.jpg'}
+        self.headers = {"Accept": "application/json","Authorization": f'OAuth {TOKEN_}'}
+        response = requests.post(url=url, params=params, headers=self.headers)
+        if response.status_code != 202:
+            print(f'Ошибка на сервере. Код ошибки: {response.status_code}')
+        else:
+            print(response)
 
 
+
+
+
+
+
+            # def upload_file(self, path, url):
+            #     #получение URL для загрузки
+            #     url = "https://cloud-api.yandex.net/v1/disk/resources/upload?path=https://disk.yandex.ru/client/disk/Photos&overwrite=false&fields=name"
+            #     headers = {'Authorization': f'OAuth {TOKEN_}'}
+            #     params = {'path': 'file', 'overwrite': 'false', 'fields': 'name'}
+            #     response = requests.get(url=url, headers=headers, params=params)
+            #     print(response)
+            #
+            #     #Загрузка файла на полученный URL
+            #     for url in urls_list:
+            #         url = "https://uploader1d.dst.yandex.net:443/upload-target/20240424T101447.217.utd.52csloukwvq67nab1yc84a3xw-k1d.6625"
+            #         response = requests.put(url=url, headers=headers, params=params)
+            #         print(response)
